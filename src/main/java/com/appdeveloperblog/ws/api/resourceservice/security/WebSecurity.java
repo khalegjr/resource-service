7db54cffc45db20e9b05354efcd4ba709a6580ea
Júnior Souza - Khaled <junior.souza@ti9.com.br>
2023-10-16 14:51:22 -0300

    Register JwtAuthenticationConverter with HttpSecurity

--- src/main/java/com/appdeveloperblog/ws/api/resourceservice/security/WebSecurity.java
+++ src/main/java/com/appdeveloperblog/ws/api/resourceservice/security/WebSecurity.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,6 +14,9 @@ public class WebSecurity {
 
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+
         // Configure Http Security
         http.authorizeHttpRequests(authz ->
                         authz
@@ -22,7 +26,8 @@ public class WebSecurity {
 //                                .hasAnyRole("developer", "user")
                                 .anyRequest()
                                 .authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}));
+                .oauth2ResourceServer(
+                        oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
 
         return http.build();
     }
