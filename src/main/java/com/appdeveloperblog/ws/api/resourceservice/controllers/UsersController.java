ab2c3b7d7b4a2dae79945bbc8ff8dbdedfdc53ae
Júnior Souza - Khaled <junior.souza@ti9.com.br>
2023-10-16 16:09:09 -0300

    PreAuthorization annotation example

--- src/main/java/com/appdeveloperblog/ws/api/resourceservice/controllers/UsersController.java
+++ src/main/java/com/appdeveloperblog/ws/api/resourceservice/controllers/UsersController.java
@@ -1,6 +1,7 @@
 package com.appdeveloperblog.ws.api.resourceservice.controllers;
 
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,7 +13,8 @@ public class UsersController {
         return "Working...";
     }
 
-    @Secured("ROLE_developer")
+//    @Secured("ROLE_developer")
+    @PreAuthorize("hasAuthority('ROLE_developer')")
     @DeleteMapping(path = "/{id}")
     public String deleteUser(@PathVariable String id) {
         return "Deleted user with id " + id;
--- src/main/java/com/appdeveloperblog/ws/api/resourceservice/security/WebSecurity.java
+++ src/main/java/com/appdeveloperblog/ws/api/resourceservice/security/WebSecurity.java
@@ -10,7 +10,7 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableMethodSecurity(securedEnabled = true)
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @EnableWebSecurity
 public class WebSecurity {
 
