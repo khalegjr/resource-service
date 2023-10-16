61bf12e97ebe2207b5441b2d93dafa3621eb44f5
Júnior Souza - Khaled <junior.souza@ti9.com.br>
2023-10-16 16:41:43 -0300

    Creating getUser() to be used with PostAuthorization annotation

--- src/main/java/com/appdeveloperblog/ws/api/resourceservice/controllers/UsersController.java
+++ src/main/java/com/appdeveloperblog/ws/api/resourceservice/controllers/UsersController.java
@@ -1,6 +1,7 @@
 package com.appdeveloperblog.ws.api.resourceservice.controllers;
 
-import org.springframework.security.access.annotation.Secured;
+import com.appdeveloperblog.ws.api.resourceservice.response.UserRest;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -20,5 +21,11 @@ public class UsersController {
     @DeleteMapping(path = "/{id}")
     public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
         return "Deleted user with id " + id + " and JWT subject " + jwt.getSubject();
+    }
+
+    @PostAuthorize("returnObject.userId == #jwt.subject")
+    @GetMapping(path = "/{id}")
+    public UserRest getUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+        return new UserRest("Khaled", "Junior", "");
     }
 }
