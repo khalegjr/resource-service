a4dd5da560354f727ef94d31217c80b9f0fe8750
Júnior Souza - Khaled <junior.souza@ti9.com.br>
2023-10-25 11:27:35 -0300

    configura para ser um Eureka Discovery Client

--- src/main/java/com/appdeveloperblog/ws/api/resourceservice/ResourceServiceApplication.java
+++ src/main/java/com/appdeveloperblog/ws/api/resourceservice/ResourceServiceApplication.java
@@ -2,7 +2,9 @@ package com.appdeveloperblog.ws.api.resourceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ResourceServiceApplication {
 
